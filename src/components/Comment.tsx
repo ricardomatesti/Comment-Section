6a6c1eaf251230df40da7e87987ed2291fca0df4
ricardomatesti <ricardosierra1145@gmail.com>
2026-01-27 13:37:21 +0100

    chore: deleted unused import

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { useIsMobile } from "../hooks/useIsMobile";
 import { useIsTextClamped } from "../hooks/useIsTextClamped";
 import { ReplyButton } from "./shared/ReplyButton";
